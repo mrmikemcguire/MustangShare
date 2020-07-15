37e4716a756c6167c05e27156d1e01e79002a23d
mrmikemcguire <moonshotmike@gmail.com>
2020-07-15 17:52:05 -0600

    initial commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isAuth = false;
+
+  buildAuthScreen() {
+    return Text('Authenticated');
+  }
+
+  buildUnauthScreen() {
+    return Scaffold(
+        body: Container(
+            child: Column(children: <Widget>[
+      Text('MullenShare',
+          style: TextStyle(
+            fontFamily: "Signatra",
+            fontSize: 90.0,
+            color: Colors.white,
+          )),
+      GestureDetector(
+          child: Container(
+        width: 260.0,
+        height: 60.0,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/google_signin_button.png'),
+                fit: BoxFit.cover)),
+      ))
+    ])));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isAuth ? buildAuthScreen() : buildUnauthScreen();
+  }
+}
